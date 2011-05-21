41aaf4db1445a84c50dfd39313f56f2e30b8bbf7
chris.nyberg <chris.nyberg@ordinal.com>
2011-05-21 00:57:24 +0000

    slightly faster version

--- mapper.py
+++ mapper.py
@@ -8,7 +8,6 @@ import sys
 import re
 
 for line in sys.stdin:
-  words = re.split(r"\W+", line)
+  words = re.findall('[A-Za-z]+', line)
   for word in words:
-    if len(word) > 0:
-      print word.lower()+'\t'+str(1)    
+    print word.lower()+'\t'+str(1)    
